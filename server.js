94d1ee3a7842d1fb05a57d7d2cc67ddf42dc2133
jinbaoxin <jinbaoxin@gmail.com>
2018-06-26 19:42:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var healthy=true;
 
 
 app.get('/', function (req, res) {
-  res.send('Hello world v.1.13 ' + os.hostname() + '\n');
+  res.send('Hello worldddddd v.1.13 ' + os.hostname() + '\n');
 });
 
 
